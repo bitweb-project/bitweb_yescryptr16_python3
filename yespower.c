b2dcae574ec6f0833ed75ade96eb9658a4432fdf
mraksoll4 <32709596+mraksoll4@users.noreply.github.com>
2023-02-16 14:13:36 +0200

    Update yespower.c

--- yespower.c
+++ yespower.c
@@ -4,6 +4,6 @@
  */
 int yespower_hash(const char *input, char *output)
 {
-	yespower_params_t params = {YESPOWER_1_0, 2048, 32, NULL, 0};
+	yespower_params_t params = {YESPOWER_0_5, 4096, 16, "Client Key", 10};
 	return yespower_tls(input, 80, &params, (yespower_binary_t *) output);
 }
