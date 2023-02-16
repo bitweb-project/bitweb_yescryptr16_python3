37816508c17a1b0fc91b66a45d95426360008cd8
mraksoll4 <32709596+mraksoll4@users.noreply.github.com>
2023-02-16 14:34:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, Extension
 
-bitweb_yespower_module = Extension('bitweb_yespower',
+bitweb_yescryptr16_module = Extension('bitweb_yescryptr16',
                             sources = ['yespower-module.c',
                                        'yespower.c',
                                        'yespower-opt.c',
@@ -9,10 +9,10 @@ bitweb_yespower_module = Extension('bitweb_yespower',
                             extra_compile_args=['-O2', '-funroll-loops', '-fomit-frame-pointer'],
                             include_dirs=['.'])
 
-setup (name = 'bitweb_yespower',
+setup (name = 'bitweb_yescryptr16',
        version = '1.0.4',
        author_email = 'mraksoll4@gmail.com',
        author = 'mraksoll',
-       url = 'https://github.com/bitweb-project/bitweb_yespower_python3',
-       description = 'Bindings for yespower-1.0 proof of work used by bitweb',
-       ext_modules = [bitweb_yespower_module])
+       url = 'https://github.com/bitweb-project/bitweb_yescryptr16_python3',
+       description = 'Bindings for yespower-0.5 or yescryptr16 proof of work used by bitweb at GPU enable update',
+       ext_modules = [bitweb_yescryptr16_module])
